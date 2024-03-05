8cdd6c2e7d9d50e850f99320a69a2e5ccb688531
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2024-03-04 20:07:55 -0500

    Prueba Sincronizacion

---
+++ EJERCICIO9/EJERCICIO9/Program.cs
@@ -0,0 +1,37 @@
+﻿using Systen;
+
+namespace Ejerrcicio9
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, num3;
+
+            Console.WriteLine("Ingresar el primer numero:  ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Ingresar el segundo numero:  ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Ingresar el tercer numero:  ");
+            num3 = Convert.ToDouble(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine($"El numero {num1} es MAYOR"); 
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine($"El numero {num2} es MAYOR");
+            }
+            else
+            {
+                Console.WriteLine($"El numero {num3} es MAYOR");
+            }
+        }
+    }
+}
+
